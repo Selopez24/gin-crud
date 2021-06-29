b0035cebe0655681b03c81bbfb021f459ef82e3c
Sebastian Lopez <sebas.lopezm24@gmail.com>
2021-06-29 11:53:24 -0500

    Add CORS config

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"github.com/Selopez24/gin-crud/handlers"
 	"github.com/Selopez24/gin-crud/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+
 	router := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+
+	corsConfig.AllowAllOrigins = true
+
+	corsConfig.AddAllowMethods("OPTIONS")
+
+	router.Use(cors.New(corsConfig))
 
 	models.ConnectDB()
 
